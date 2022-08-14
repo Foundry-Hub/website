767b9d791c0230a5fd6eeaeca2ad7a93588c6ce4
Mathias Latournerie <jidewe@gmail.com>
2022-08-14 23:04:22 +0200

    add jamconfig

---
+++ htdocs/JamConfig.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+__Categories__
+Best Package
+Useful
+Polished
+Mind-blowing
+Wacky
+Gorgeous
+Massive
+Educational
+Integratable
+
+*/
+
+define('PACKAGE_JAM_CATEGORIES', [
+    "best-package" => "Best Package",
+    "useful" => "Useful",
+    "polished" => "Polished",
+    "mind-blowing" => "Mind-blowing",
+    "wacky" => "Wacky",
+    "gorgeous" => "Gorgeous",
+    "massive" => "Massive",
+    "educational" => "Educational",
+    "integratable" => "Integratable",
+    "first-package" => "First Package"
+]);
+
+define('MAX_VOTES_PER_CATEGORY', 3);
+
+//Set the packages nominated for each category
+
+define('PACKAGE_JAM_CATEGORIES_NOMINATIONS', [
+    "best-package" => [
+        "package-1",
+        "package-2",
+        "package-3"
+    ],
+    "useful" => [
+        "package-4",
+        "package-5",
+        "package-6"
+    ],
+    "polished" => [
+        "package-7",
+        "package-8",
+        "package-9"
+    ],
+    "mind-blowing" => [
+        "package-10",
+        "package-11",
+        "package-12"
+    ],
+    "wacky" => [
+        "package-13",
+        "package-14",
+        "package-15"
+    ],
+    "gorgeous" => [
+        "package-16",
+        "package-17",
+        "package-18"
+    ],
+    "massive" => [
+        "package-19",
+        "package-20",
+        "package-21"
+    ],
+    "educational" => [
+        "package-22",
+        "package-23",
+        "package-24"
+    ],
+    "integratable" => [
+        "package-25",
+        "package-26",
+        "package-27"
+    ],
+    "first-package" => [
+        "package-28",
+        "package-29",
+        "package-30"
+    ]
+]);
+
